ec8bc1c1a358b2e63ec5f9eb579adda676158ea4
pavvlik777 <pavvlik77727@gmail.com>
2026-04-27 18:59:23 +0300

    fix files

--- task_1/src/components/icons/IconChevron.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <svg :width="size" :height="size" viewBox="0 0 2048 2048">
-    <path fill="currentColor" d="m1939 467l90 90l-1005 1005L19 557l90-90l915 915z" />
-  </svg>
-</template>
-
-<script setup lang="ts">
-withDefaults(defineProps<{ size?: number }>(), { size: 20 })
-</script>
--- task_1/src/components/icons/IconStar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <svg :width="size" :height="size" viewBox="0 0 2048 2048" fill="currentColor">
-    <path d="M1024 0l316 684 736 84-552 492 156 728-656-372-656 372 156-728L-28 768l736-84L1024 0z" />
-  </svg>
-</template>
-
-<script setup lang="ts">
-withDefaults(defineProps<{ size?: number }>(), { size: 28 })
-</script>
